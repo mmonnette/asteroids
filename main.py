034f4b59d44c6889ce5b627dfee1418eadefe004
mmonnette <monnette.marshall@gmail.com>
2024-08-20 17:36:16 -0400

    Created player and circle object class

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import pygame
 from constants import *
+from circleshape import CircleShape
+from player import Player
 
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
 
     while True:
+        #check for window closure
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
+        #draw current game state on screen    
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+
+        #limit framerate to 60 fps
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
